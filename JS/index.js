82f43b98a65a99b11ecc385f4db07edc41a05b84
= <ismoilbek20050421@gmail.com>
2022-08-13 13:07:53 +0500

    Dwel

--- JS/index.js
+++ JS/index.js
@@ -1,35 +1,49 @@
-let times = document.querySelector(".fa-times");
-let newNavbar = document.querySelector(".nav__new_navbar");
-let faBars = document.querySelector(".fa-bars-staggered");
-let header = document.querySelector(".header");
-let images = document.querySelectorAll(".section__box_small img");
-let timesSecret = document.querySelector(".section__secret_img_hover i");
-let secretPicture = document.querySelector(".section__secret_img");
-let secretPictureImg = document.querySelector(".section__secret_img_hover img");
+window.addEventListener("DOMContentLoaded", () => {
+  let times = document.querySelector(".fa-times");
+  let newNavbar = document.querySelector(".nav__new_navbar");
+  let faBars = document.querySelector(".fa-bars-staggered");
+  let header = document.querySelector(".header");
+  let images = document.querySelectorAll(".section__box_small img");
+  let timesSecret = document.querySelector(".section__secret_img_hover i");
+  let secretPicture = document.querySelector(".section__secret_img");
+  let secretPictureImg = document.querySelector(
+    ".section__secret_img_hover img"
+  );
+  let body = document.querySelector("body");
+  let newContainerNavbar = document.querySelector(".new__navbar_container");
 
-window.addEventListener("scroll", () => {
-  header.classList.toggle("header__scroll", window.scrollY > 0);
+  window.addEventListener("scroll", () => {
+    header.classList.toggle("header__scroll", window.scrollY > 0);
+  });
+
+  faBars.addEventListener("click", () => {
+    newNavbar.classList.add("active");
+    body.style.overflow = "hidden";
+    newContainerNavbar.classList.add("active");
+  });
+
+  times.addEventListener("click", () => {
+    newNavbar.classList.remove("active");
+    body.style.overflow = "";
+    newContainerNavbar.classList.remove("active");
+  });
+
+  images.forEach((element) => {
+    element.addEventListener("click", () => {
+      secretPicture.style.transform = "scale(1.0)";
+      secretPictureImg.src = element.getAttribute("src");
+    });
+  });
+
+  timesSecret.addEventListener("click", () => {
+    secretPicture.style.transform = "scale(0)";
+  });
+
+  newContainerNavbar.addEventListener("click", (event) => {
+    if (event.target == newContainerNavbar) {
+      newNavbar.classList.remove("active");
+      body.style.overflow = "";
+      newContainerNavbar.classList.remove("active");
+    }
+  });
 });
-
-times.addEventListener("click", () => {
-  newNavbar.style.transform = "translateX(-675px)";
-  newNavbar.style.boxShadow = "0 0 0 white";
-  console.log(1);
-});
-
-faBars.addEventListener("click", () => {
-  newNavbar.style.transform = "translateX(0px)";
-  newNavbar.style.boxShadow = "0 0 20px 1000px rgba(0, 0, 0,0.7)";
-  console.log(1);
-});
-
-images.forEach(element => {
-  element.addEventListener("click",()=>{
-    secretPicture.style.transform = "scale(1.0)";
-    secretPictureImg.src = element.getAttribute("src");
-  })
-})
-
-timesSecret.addEventListener("click",()=>{
-  secretPicture.style.transform = "scale(0)";
-})
