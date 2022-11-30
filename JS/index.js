19b7d33c72cc904cbf617a352cfe17d02de31441
= <ismoilbek20050421@gmail.com>
2022-11-30 22:41:45 +0500

    Dwel The End

--- JS/index.js
+++ JS/index.js
@@ -1,89 +1,74 @@
 window.addEventListener("DOMContentLoaded", () => {
-  let times = document.querySelector(".fa-times");
-  let newNavbar = document.querySelector(".nav__new_navbar");
-  let faBars = document.querySelector(".fa-bars-staggered");
-  let header = document.querySelector(".header");
-  let images = document.querySelectorAll(".section__box_small img");
-  let secretPicture = document.querySelector(".section__secret_img");
-  let secretPictureImg = document.querySelector(
-    ".section__secret_img_hover img"
-  );
-  let section__secret_img_hover = document.querySelector(
-    ".section__secret_img_hover"
-  );
-  let body = document.querySelector("body");
-  let newContainerNavbar = document.querySelector(".new__navbar_container");
-  let newNavbarLinks = newContainerNavbar.querySelectorAll(
-    ".nav__new_navbar_link"
-  );
 
-  // Sroll
+  // ****SCROLL START****
   window.addEventListener("scroll", () => {
-    header.classList.toggle("header__scroll", window.scrollY > 0);
+    $(".header").classList.toggle("header__scroll", window.scrollY > 0);
   });
+  // ****SCROLL END****
 
-  // Open Navbar Function
+  // ****NAVBAR FUNCTION START****
   function openNavbarFunction() {
-    newNavbar.classList.add("active");
-    newContainerNavbar.classList.add("active");
+    $(".nav__new_navbar").classList.add("active");
+    $(".new__navbar_container").classList.add("active");
   }
 
-  // Close Navbar Function
   function closeNavbarFunction() {
-    newNavbar.classList.remove("active");
-    newContainerNavbar.classList.remove("active");
+    $(".nav__new_navbar").classList.remove("active");
+    $(".new__navbar_container").classList.remove("active");
   }
 
-  // Close Picture Function
-  function closePicture() {
-    secretPicture.classList.remove("active");
-    body.style.overflow = "";
-  }
-
-  // Bars
-  faBars.addEventListener("click", () => {
+  $(".fa-bars-staggered").addEventListener("click", () => {
     openNavbarFunction();
   });
 
-  // Times
-  times.addEventListener("click", () => {
+  $(".fa-times").addEventListener("click", () => {
     closeNavbarFunction();
   });
 
-  // Images
-  images.forEach((element) => {
-    element.addEventListener("click", () => {
-      secretPicture.classList.add("active");
-      body.style.overflow = "hidden";
-      secretPictureImg.src = element.getAttribute("src");
-    });
-  });
-
-  // New Navbar Container
-  newContainerNavbar.addEventListener("click", (event) => {
-    if (event.target == newContainerNavbar) {
-      closeNavbarFunction();
-    }
-  });
-
-  // Secret Img
-  section__secret_img_hover.addEventListener("click", (event) => {
-    if (event.target == section__secret_img_hover) {
-      closePicture();
-    }
-  });
-
-  // New Navbar Links
-  newNavbarLinks.forEach((link) => {
+  $$(".nav__new_navbar_link").forEach((link) => {
     link.addEventListener("click", () => {
       closeNavbarFunction();
     });
   });
 
+  
   this.addEventListener("keydown", (e) => {
     if (e.code == "Escape") {
       closeNavbarFunction();
       closePicture();
     }
   });
+
+  // ****NAVBAR FUNCTION END****
+
+
+  // ****PICTURE CODES START****
+
+  function closePicture() {
+    $(".section__secret_img").classList.remove("active");
+    $("body").style.overflow = "";
+  }
+
+
+  $$(".section__box_small img").forEach((element) => {
+    element.addEventListener("click", () => {
+      $(".section__secret_img").classList.add("active");
+      $("body").style.overflow = "hidden";
+      $(".section__secret_img_hover img").src = element.getAttribute("src");
+    });
+  });
+
+  $(".new__navbar_container").addEventListener("click", (e) => {
+    if (e.target == $(".new__navbar_container")) {
+      closeNavbarFunction();
+    }
+  });
+
+  $(".section__secret_img_hover").addEventListener("click", (e) => {
+    if (e.target == $(".section__secret_img_hover")) {
+      closePicture();
+    }
+  });
+
+  // ****PICTURE CODES END****
 });
